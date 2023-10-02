27605dae0133c3428d42f689a1d1cf4b1f30a336
Harikrishna-AL <amenu4aie21034@am.students.amrita.edu>
2023-10-02 11:54:06 +0530

    add: RL_agent_class and its base class

--- cart.py
+++ cart.py
@@ -1,15 +1,22 @@
 import gymnasium as gym
 from tqdm import tqdm
+import numpy as np
+from gym.utils.play import play
 
-env = gym.make('CartPole-v1',render_mode="human")
+
+env = gym.make("CartPole-v1", render_mode="human")
 obs, info = env.reset()
 
-for _ in tqdm(range(1000)):
+for _ in tqdm(range(500)):
     action = env.action_space.sample()
+    # print(type(action))
     obs, reward, done, truncated, info = env.step(action)
-    print(info)
-    if done:
+    # print(obs, action)
+    # print(info)
+    if done or truncated:
         obs, info = env.reset()
 
 env.close()
 # env.render()
+
+# gym.utils.play.play(env, zoom=3)
--- pi_model.py
+++ pi_model.py
@@ -8,10 +8,10 @@ import torch.nn as nn
 def pos_table(n, dim):
     def get_angle(x, h):
         return x / np.power(10000, 2 * (h // 2) / dim)
-    
+
     def get_angle_vec(x):
         return [get_angle(x, h) for h in range(dim)]
-    
+
     tab = np.array([get_angle_vec(i) for i in range(n)])
     tab[:, 0::2] = np.sin(tab[:, 0::2])
     tab[:, 1::2] = np.cos(tab[:, 1::2])
@@ -27,7 +27,7 @@ class AttentionMatrix(nn.Module):
             self.msg_dim = msg_dim
         else:
             self.msg_dim = 1
-        
+
     def forward(self, q, k):
         q = self.proj_q(q)
         k = self.proj_k(k)
@@ -36,7 +36,7 @@ class AttentionMatrix(nn.Module):
         else:
             dot = torch.bmm(q, k.permute(0, 2, 1))
         return torch.div(dot, np.sqrt(self.msg_dim))
-        
+
 
 class SelfAttentionMatrix(nn.Module):
     def __init__(self, dim_in, msg_dim, bias=True, scale=True):
@@ -50,13 +50,9 @@ class SelfAttentionMatrix(nn.Module):
 
 
 class AttentionNeuron(nn.Module):
-    def __init__(self,
-                 act_dim,
-                 hidden_dim,
-                 msg_dim,
-                 pos_emb_dim,
-                 bias=True,
-                 scale=True):
+    def __init__(
+        self, act_dim, hidden_dim, msg_dim, pos_emb_dim, bias=True, scale=True
+    ):
         super(AttentionNeuron, self).__init__()
         self.act_dim = act_dim
         self.hidden_dim = hidden_dim
@@ -64,11 +60,9 @@ class AttentionNeuron(nn.Module):
         self.pos_emb_dim = pos_emb_dim
         self.pos_embedding = torch.from_numpy(
             pos_table(self.act_dim, self.pos_emb_dim)
-            ).float()
+        ).float()
         self.hx = None
-        self.lstm = nn.LSTMCell(
-            input_size=1 + self.act_dim, hidden_size=pos_emb_dim
-        )
+        self.lstm = nn.LSTMCell(input_size=1 + self.act_dim, hidden_size=pos_emb_dim)
         self.attention = SelfAttentionMatrix(
             dim_in=pos_emb_dim,
             msg_dim=msg_dim,
@@ -91,14 +85,113 @@ class AttentionNeuron(nn.Module):
             )
         self.hx = self.lstm(x_aug, self.hx)
 
-        w = torch.tanh(self.attention(
-            q=self.pos_embedding.to(x.device), k=self.hx[0]
-        ))
+        w = torch.tanh(self.attention(q=self.pos_embedding.to(x.device), k=self.hx[0]))
         output = torch.matmul(w, x)
         return torch.tanh(output)
-    
+
     def reset(self):
         self.hx = None
-        
-            
-        
+
+
+class BasePiTorchModel(BasePiModel):
+    def __init__(self, device):
+        self.modules_to_learn = []
+        self.device = torch.device(device)
+
+    def get_action(self, obs):
+        with torch.no_grad():
+            return self._get_action(obs)
+
+    def get_params(self):
+        params = []
+        with torch.no_grad():
+            for layer in self.modules_to_learn:
+                for p in layer.parameters():
+                    params.append(p.cpu().numpy().ravel())
+        return np.concatenate(params)
+
+    def set_params(self, params):
+        assert isinstance(params, np.ndarray)
+        ss = 0
+        for layer in self.modules_to_learn:
+            for p in layer.parameters():
+                ee = ss + np.prod(p.shape)
+                p.data = (
+                    torch.from_numpy(params[ss:ee].reshape(p.shape))
+                    .float()
+                    .to(self.device)
+                )
+                ss = ee
+        assert ss == params.size
+
+    def save(self, filename):
+        params = self.get_params()
+        np.savez(filename, params=params)
+
+    def load(self, filename):
+        with np.load(filename) as data:
+            params = data["params"]
+            self.set_params(params)
+
+    def get_num_params(self):
+        return self.get_params().size
+
+    def _get_action(self, obs):
+        raise NotImplementedError()
+
+    def reset(self):
+        pass
+
+
+class RL_agent(BasePiTorchModel):
+    def __init__(
+        self,
+        device,
+        act_dim,
+        hidden_dim,
+        msg_dim,
+        pos_em_dim,
+        num_hidden_layers=1,
+        pi_layer_bias=True,
+        pi_layer_scale=True,
+    ):
+        super(RL_agent, self).__init__(device=device)
+        self.act_dim = act_dim
+        self.hidden_dim = hidden_dim
+        self.msg_dim = msg_dim
+        self.pos_em_dim = pos_em_dim
+        self.prev_act = torch.zeros(1, self.act_dim)
+
+        self.att_neuron = AttentionNeuron(
+            act_dim=act_dim,
+            hidden_dim=hidden_dim,
+            msg_dim=msg_dim,
+            pos_emb_dim=pos_em_dim,
+            bias=pi_layer_bias,
+            scale=pi_layer_scale,
+        )
+        self.modules_to_learn.append(self.pi_layer)
+
+        hidden_lyrs = []
+        for layer in range(num_hidden_layers):
+            hidden_lyrs.extend(
+                [
+                    nn.Linear(in_features=hidden_dim, out_features=hidden_dim),
+                    nn.Tanh(),
+                ]
+            )
+        self.net = nn.Sequential(
+            *hidden_lyrs,
+            nn.Linear(in_features=hidden_dim, out_features=hidden_dim),
+            nn.Tanh(),
+        )
+        self.modules_to_learn.append(self.net)
+
+    def _get_action(self, obs):
+        x = self.att_neuron(obs=obs, prev_act=self.prev_act)
+        self.prev_act = self.net(x.T)
+        return self.prev_act.squeeze(0).cpu().numpy()
+
+    def reset(self):
+        self.prev_act = torch.zeros(1, self.act_dim)
+        self.att_neuron.reset()
